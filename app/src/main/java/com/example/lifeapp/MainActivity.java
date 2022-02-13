93a40023d4a9fbb5b08a0a6d1d4d1c8ff9e8c9cf
leaburriat <lea.burriat@epfedu.fr>
2022-02-13 12:30:07 +0100

    changement d'activité au clic du boutton Enter --> passe du MainActivity à l'écran d'Acceuil

--- app/src/main/java/com/example/lifeapp/MainActivity.java
+++ app/src/main/java/com/example/lifeapp/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.lifeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAcceuil();
+            }
+        });
+    }
+
+    public void openAcceuil() {
+        Intent intent = new Intent(this, Acceuil.class);
+        startActivity(intent);
     }
 }
