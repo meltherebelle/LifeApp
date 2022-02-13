142104366ec4d9e44261b3e91ec33d9f2f7af4b7
leaburriat <lea.burriat@epfedu.fr>
2022-02-13 12:02:26 +0100

    Initial commit Création des activitées de base

---
+++ app/src/main/java/com/example/lifeapp/Acceuil.java
@@ -0,0 +1,14 @@
+package com.example.lifeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Acceuil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+    }
+}
