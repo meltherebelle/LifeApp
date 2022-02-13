4c7b3a55f7645f2cd69885c570f6ddf3cade2b47
leaburriat <lea.burriat@epfedu.fr>
2022-02-13 12:57:34 +0100

    changement activité --> returnBtn pas encore fonctionnel (pb d'ouverture app dans émulator)

--- app/src/main/java/com/example/lifeapp/MainActivity.java
+++ app/src/main/java/com/example/lifeapp/MainActivity.java
@@ -10,13 +10,22 @@ import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private Button returnBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
+        returnBtn = (Button) findViewById(R.id.returnBtn);
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+
+        button = (Button) findViewById(R.id.resultsBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void openAcceuil() {
         Intent intent = new Intent(this, Acceuil.class);
+        startActivity(intent);
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
