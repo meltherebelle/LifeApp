dea177e134a1d81446d7dcb4a2c946a13539f317
delph <delphine.ringuede02@gmail.com>
2022-02-22 11:10:26 +0100

    Part Work : ReturnBtn2

--- app/src/main/java/com/example/lifeapp/Work.java
+++ app/src/main/java/com/example/lifeapp/Work.java
@@ -1,14 +1,34 @@
 package com.example.lifeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 import android.os.Bundle;
 
 public class Work extends AppCompatActivity {
 
+    Button ReturnBtn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work);
+
+        //retourner sur l'acceuil au clic du bouton Return
+        ReturnBtn2 = (Button) findViewById(R.id.ReturnBtn2);
+        ReturnBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAcceuil();
+            }
+        });
     }
+
+    public void openAcceuil() {
+        Intent intent = new Intent(this, Acceuil.class);
+        startActivity(intent);
+    }
+
 }
