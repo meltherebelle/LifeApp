5a7389d1b5f3ec406f11a4892e4de16ef300c866
leaburriat <lea.burriat@epfedu.fr>
2022-02-22 11:18:16 +0100

    actions boutons dans Perso

--- app/src/main/java/com/example/lifeapp/Perso.java
+++ app/src/main/java/com/example/lifeapp/Perso.java
@@ -9,6 +9,10 @@ import android.os.Bundle;
 public class Perso extends AppCompatActivity {
 
     Button Return1Btn;
+    Button MoodBtn;
+    Button ImBoredBtn;
+    Button HobbiesBtn;
+    Button GoalsPersoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +27,66 @@ public class Perso extends AppCompatActivity {
                 openAcceuil();
             }
         });
+
+        //aller sur écran Mood au clic du bouton MoodBtn
+        MoodBtn = (Button) findViewById(R.id.MoodBtn);
+        MoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMood();
+            }
+        });
+
+        //aller sur écran ImBored au clic du bouton ImBoredBtn
+        ImBoredBtn = (Button) findViewById(R.id.ImBoredBtn);
+        ImBoredBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImBored();
+            }
+        });
+
+        //aller sur écran Hobbies au clic du bouton HobbiesBtn
+        HobbiesBtn = (Button) findViewById(R.id.HobbiesBtn);
+        HobbiesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHobbies();
+            }
+        });
+
+        //aller sur écran GoalsPerso au clic du bouton GoalsPersoBtn
+        GoalsPersoBtn = (Button) findViewById(R.id.GoalsPersoBtn);
+        GoalsPersoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGoalsPerso();
+            }
+        });
+
     }
     public void openAcceuil () {
         Intent intent = new Intent(this, Acceuil.class);
+        startActivity(intent);
+    }
+
+    public void openMood () {
+        Intent intent = new Intent(this, Mood.class);
+        startActivity(intent);
+    }
+
+    public void openImBored () {
+        Intent intent = new Intent(this, ImBored.class);
+        startActivity(intent);
+    }
+
+    public void openHobbies () {
+        Intent intent = new Intent(this, Hobbies.class);
+        startActivity(intent);
+    }
+
+    public void openGoalsPerso () {
+        Intent intent = new Intent(this, GoalsPerso.class);
         startActivity(intent);
     }
 }
