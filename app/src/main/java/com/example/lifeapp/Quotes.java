2eeaf344621fc4a60620f03b3beb01cfbfe12bf9
delph <delphine.ringuede02@gmail.com>
2022-03-18 11:17:55 +0100

    essai de push

---
+++ app/src/main/java/com/example/lifeapp/Quotes.java
@@ -0,0 +1,4 @@
+package com.example.lifeapp;
+
+public class Quotes {
+}
--- app/src/main/java/com/example/lifeapp/Work.java
+++ app/src/main/java/com/example/lifeapp/Work.java
@@ -10,13 +10,15 @@ import android.os.Bundle;
 public class Work extends AppCompatActivity {
 
     Button ReturnBtn2;
+    Button Pomodoro;
+    Button Goals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work);
 
-        //retourner sur l'acceuil au clic du bouton Return
+        /* retourner sur l'acceuil au clic du bouton Return */
         ReturnBtn2 = (Button) findViewById(R.id.ReturnBtn2);
         ReturnBtn2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,10 +26,38 @@ public class Work extends AppCompatActivity {
                 openAcceuil();
             }
         });
+
+        /* aller sur la page Pomodoro au clic du bouton Pomodoro */
+        Pomodoro = (Button) findViewById(R.id.Pomodoro);
+        Pomodoro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPomodoro();
+            }
+        });
+
+        /* aller sur la page Goals au clic du bouton Goals */
+        Goals = (Button) findViewById(R.id.Goals);
+        Goals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGoals();
+            }
+        });
     }
 
     public void openAcceuil() {
         Intent intent = new Intent(this, Acceuil.class);
+        startActivity(intent);
+    }
+
+    public void openPomodoro() {
+        Intent intent = new Intent(this, Pomodoro.class);
+        startActivity(intent);
+    }
+
+    public void openGoals() {
+        Intent intent = new Intent(this, Pomodoro.class);
         startActivity(intent);
     }
 
